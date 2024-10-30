10606da5c3e12bf4f3aca60a261d8c69a43cfc20
JAAGRIT <jaagrit08@gmail.com>
2024-10-30 20:46:40 +0530

    installed react-router-dom and its basic routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,19 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Starred from './pages/Starred';
 function App() {
-  return <div>hello</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/contact" element={<div>contact page</div>} />
+          <Route path="/starred" element={<Starred />} />
+          <Route path="*" element={<div>Page Not Found</div>} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
