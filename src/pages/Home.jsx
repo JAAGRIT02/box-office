86f51df20370319676fdfa9e3b3da1b2d0bf3ca3
JAAGRIT <jaagrit08@gmail.com>
2024-10-31 10:01:32 +0530

    one/two way data binding

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,31 @@
 // import { Link } from 'react-router-dom';
 
+import { useState } from 'react';
+
 export default function Home() {
+  const [inputValue, setInputValue] = useState('');
+  const onInputChange = evt => {
+    // console.log(evt.target.value);
+    // console.log(inputValue);
+
+    setInputValue(evt.target.value);
+  };
   return (
     <div>
-      Welcome to Home Page <br />
+      Welcome to Home Page <br /> <br />
+      <div>{inputValue}</div>
+      
+      {/* one way data binding as value is not changing in textarea on click*/}
+      <input type="text" onChange={onInputChange} value={inputValue} />
+      {/* two way data binding by putting value attribute in input */}
+
+      <button
+        onClick={() => {
+          setInputValue('Jaagrit');
+        }}
+      >
+        click to update to random value
+      </button>
       {/* <Link to="/starred">Go to Starred page</Link> */}
     </div>
   );
