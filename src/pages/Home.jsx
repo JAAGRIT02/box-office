ccfd43e54e7d14391a1928295e918c855ef84bee
JAAGRIT <jaagrit08@gmail.com>
2024-10-31 11:56:03 +0530

    added tv maze api for search tv shows

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,29 +3,26 @@
 import { useState } from 'react';
 
 export default function Home() {
-  const [inputValue, setInputValue] = useState('');
+  const [searchString, setSearchString] = useState('');
+
   const onInputChange = evt => {
     // console.log(evt.target.value);
-    // console.log(inputValue);
-
-    setInputValue(evt.target.value);
+    // console.log(searchString);
+    setSearchString(evt.target.value);
+  };
+  const onSearch = async evt => {
+    evt.preventDefault();
+    const response = await fetch(`https://api.tvmaze.com/search/shows?q=${searchString}`);
+    const body = await response.json();
+    console.log(body);
   };
   return (
     <div>
       Welcome to Home Page <br /> <br />
-      <div>{inputValue}</div>
-      
-      {/* one way data binding as value is not changing in textarea on click*/}
-      <input type="text" onChange={onInputChange} value={inputValue} />
-      {/* two way data binding by putting value attribute in input */}
-
-      <button
-        onClick={() => {
-          setInputValue('Jaagrit');
-        }}
-      >
-        click to update to random value
-      </button>
+      <form onSubmit={onSearch}>
+        <input type="text" onChange={onInputChange} value={searchString} />
+        <button type="submit">Search</button>
+      </form>
       {/* <Link to="/starred">Go to Starred page</Link> */}
     </div>
   );
