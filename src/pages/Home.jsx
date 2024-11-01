ac64124c9a45649228120f49a201d6797c3c0c67
JAAGRIT <jaagrit08@gmail.com>
2024-11-01 19:46:06 +0530

    added search for actors and radio button to switch

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,47 +1,81 @@
 // import { Link } from 'react-router-dom';
 
 import { useState } from 'react';
-import { searchForShows } from '../api/tvMaze';
+import { searchForPeople, searchForShows } from '../api/tvMaze';
 
 export default function Home() {
   const [searchString, setSearchString] = useState('');
   const [apiData, setApiData] = useState(null);
   const [apiDataError, setApiDataError] = useState(null);
-
+  const [searchOption, setSearchOption] = useState('shows');
   // console.log(apiDataError);
+  // console.log(searchOption);
 
   const onInputChange = evt => {
     // console.log(evt.target.value);
     // console.log(searchString);
     setSearchString(evt.target.value);
   };
+
   const onSearch = async evt => {
-    setApiDataError(null); //used to clean up previous error if there any
     evt.preventDefault();
     try {
-      const result = await searchForShows(searchString);
+      setApiDataError(null); //used to clean up previous error if there any
+      if (searchOption === 'shows') {
+        const result = await searchForShows(searchString);
+        setApiData(result);
+      } else {
+        const result = await searchForPeople(searchString);
+        setApiData(result);
+      }
+
       // console.log(result);
-      setApiData(result);
     } catch (error) {
       setApiDataError(error);
     }
   };
+
+  const onRadioChange = evt => {
+    setSearchOption(evt.target.value);
+  };
+
   const renderData = () => {
     if (apiDataError) {
       return <div>Error occured: {apiDataError.message}</div>;
     }
     if (apiData) {
-      return apiData.map(data => (
-        <div key={data.show.id}>{data.show.name}</div>
-      ));
+      return apiData[0].show
+        ? apiData.map(data => <div key={data.show.id}>{data.show.name}</div>)
+        : apiData.map(data => <div key={data.person.id}>{data.person.name}</div>);
     }
     return null;
   };
+
   return (
     <div>
       Welcome to Home Page <br /> <br />
       <form onSubmit={onSearch}>
         <input type="text" onChange={onInputChange} value={searchString} />
+        <label htmlFor="">
+          Shows
+          <input
+            type="radio"
+            value="shows"
+            name="search-option"
+            onChange={onRadioChange}
+            checked={searchOption === 'shows'}
+          />
+        </label>
+        <label htmlFor="">
+          Actors
+          <input
+            type="radio"
+            value="actors"
+            name="search-option"
+            onChange={onRadioChange}
+            checked={searchOption === 'actors'}
+          />
+        </label>
         <button type="submit">Search</button>
       </form>
       <div>{renderData()}</div>
