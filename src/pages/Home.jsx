056f78a6468d465d0a86ff884cbaa061b7173b45
JAAGRIT <jaagrit08@gmail.com>
2024-11-03 17:38:51 +0530

    used useQuery on homepage to fetch search data

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,26 +5,42 @@ import { searchForPeople, searchForShows } from '../api/tvMaze';
 import SearchForm from '../components/SearchFrom';
 import ShowGrid from '../components/shows/ShowGrid';
 import ActorGrid from '../components/actors/ActorGrid';
+import { useQuery } from 'react-query';
 
 export default function Home() {
-  const [apiData, setApiData] = useState(null);
-  const [apiDataError, setApiDataError] = useState(null);
+  const [filter, setFilter] = useState(null);
+
+  const { data: apiData, error: apiDataError } = useQuery({
+    queryKey: ['search', filter],
+    queryFn: () =>
+      filter.searchOption === 'shows'
+        ? searchForShows(filter.q)
+        : searchForPeople(filter.q),
+    // ⬇️ disabled as long as the filter is empty
+    enabled: !!filter,
+    refetchOnWindowFocus: false,
+  });
+
+  // const [apiData, setApiData] = useState(null);
+  // const [apiDataError, setApiDataError] = useState(null);
 
   const onSearch = async ({ q, searchOption }) => {
-    try {
-      setApiDataError(null); //used to clean up previous error if there any
-      let result;
-      if (searchOption === 'shows') {
-        result = await searchForShows(q);
-      } else {
-        result = await searchForPeople(q);
-      }
-      setApiData(result);
+    setFilter({ q, searchOption });
 
-      // console.log(result);
-    } catch (error) {
-      setApiDataError(error);
-    }
+    // try {
+    //   setApiDataError(null); //used to clean up previous error if there any
+    //   let result;
+    //   if (searchOption === 'shows') {
+    //     result = await searchForShows(q);
+    //   } else {
+    //     result = await searchForPeople(q);
+    //   }
+    //   setApiData(result);
+
+    //   // console.log(result);
+    // } catch (error) {
+    //   setApiDataError(error);
+    // }
   };
 
   const renderData = () => {
