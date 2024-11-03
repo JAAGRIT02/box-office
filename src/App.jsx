c65dd51e766a8020bf4bd22c02e6337906544857
JAAGRIT <jaagrit08@gmail.com>
2024-11-03 17:04:53 +0530

    installed react-query,replaced useShowById to useQuery hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,28 @@
+import { QueryClient, QueryClientProvider } from 'react-query';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Starred from './pages/Starred';
 import MainLayout from './components/MainLayout';
 import Show from './pages/Show';
+
+const queryClient = new QueryClient();
+
 function App() {
   return (
     <div>
-      <BrowserRouter>
-        <Routes>
-          <Route element={<MainLayout />}>
-            <Route path="/" element={<Home />} />
-            <Route path="/starred" element={<Starred />} />
-          </Route>
-          <Route path="/show/:showId" element={<Show />} />
-          <Route path="/contact" element={<div>contact page</div>} />
-          <Route path="*" element={<div>Page Not Found</div>} />
-        </Routes>
-      </BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <Routes>
+            <Route element={<MainLayout />}>
+              <Route path="/" element={<Home />} />
+              <Route path="/starred" element={<Starred />} />
+            </Route>
+            <Route path="/show/:showId" element={<Show />} />
+            <Route path="/contact" element={<div>contact page</div>} />
+            <Route path="*" element={<div>Page Not Found</div>} />
+          </Routes>
+        </BrowserRouter>
+      </QueryClientProvider>
     </div>
   );
 }
--- src/pages/Show.jsx
+++ src/pages/Show.jsx
@@ -1,32 +1,16 @@
-import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { getShowById } from '../api/tvMaze';
-
-const useShowById = showId => {
-  const [showData, setShowData] = useState(null);
-  const [showError, setShowError] = useState(null);
-
-  useEffect(() => {
-    // getShowById(showId)
-    async function fetchData() {
-      try {
-        const data = await getShowById(showId);
-        // console.log(data);
-        setShowData(data);
-      } catch (error) {
-        setShowError(error);
-      }
-    }
-    fetchData();
-  }, [showId]);
-  return { showData, showError };
-};
+import { useQuery } from 'react-query';
 
 export default function Show() {
   const { showId } = useParams();
 
-  const { showData, showError } = useShowById(showId);
-
+  // const { showData, showError } = useShowById(showId);
+  const { data: showData, error: showError } = useQuery({
+    //recommended way of fetching the data
+    queryKey: ['show', showId],
+    queryFn: () => getShowById(showId),
+  });
   if (showError) {
     return <div>Got an Error : {showError.message}</div>;
   }
@@ -36,3 +20,24 @@ export default function Show() {
 
   return <div>Data is loading</div>;
 }
+
+// const useShowById = showId => {
+//   const [showData, setShowData] = useState(null);
+//   const [showError, setShowError] = useState(null);
+
+// useEffect(() => {   //not recommended when in strict mode
+//   // getShowById(showId)
+//   async function fetchData() {
+//     try {
+//       const data = await getShowById(showId);
+//       // console.log(data);
+//       setShowData(data);
+//     } catch (error) {
+//       setShowError(error);
+//     }
+//   }
+//   fetchData();
+// }, [showId]);
+
+//   return { showData, showError };
+// };
