97b5e3bb03fdfcb9cf921ea5ce15b71d9319a451
JAAGRIT <jaagrit08@gmail.com>
2024-11-05 19:22:24 +0530

    fetched starred show data and display show grid for them

--- src/api/tvMaze.js
+++ src/api/tvMaze.js
@@ -12,3 +12,8 @@ export const searchForShows = query => apiGet(`/search/shows?q=${query}`);
 export const searchForPeople = query => apiGet(`/search/people?q=${query}`);
 export const getShowById = showId =>
   apiGet(`/shows/${showId}?embed[]=seasons&embed[]=cast`);
+
+export const getShowsbyIds = async showIds => {
+  const promises = showIds.map(showId => apiGet(`/shows/${showId}`));
+  return Promise.all(promises);
+};
--- src/pages/Starred.jsx
+++ src/pages/Starred.jsx
@@ -1,6 +1,31 @@
+import { useQuery } from 'react-query';
+import { getShowsbyIds } from '../api/tvMaze';
+import ShowGrid from '../components/shows/ShowGrid';
 import { useStarredShows } from '../lib/useStarredShows';
 
 export default function Starred() {
-  const [starred] = useStarredShows();
-  return <div>Starred page ,starred{starred.length}</div>;
+  const [starredShowIds] = useStarredShows();
+
+  const { data: starredShow, error: starredShowError } = useQuery({
+    queryKey: ['starred', starredShowIds],
+    queryFn: async () =>
+      getShowsbyIds(starredShowIds).then(result =>
+        result.map(show => ({ show }))
+      ),
+    refetchOnWindowFocus: false,
+  });
+  console.log(starredShow);
+
+  if (starredShow?.length === 0) {
+    return <div>No shows are starred</div>;
+  }
+
+  if (starredShow?.length > 0) {
+    return <ShowGrid show={starredShow} />;
+  }
+  if (starredShowError) {
+    return <div>Error occured{starredShowError.message}</div>;
+  }
+
+  return <div>Shows are still loading</div>;
 }
