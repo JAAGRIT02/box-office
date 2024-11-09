011e44570cb5a587210e63358e9502808ac1bbe0
JAAGRIT <jaagrit08@gmail.com>
2024-11-09 13:11:58 +0530

    created globalTheme and CustomRadio component for styling search form.

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Home from './pages/Home';
 import Starred from './pages/Starred';
 import MainLayout from './components/MainLayout';
 import Show from './pages/Show';
+import { GlobalTheme } from './Theme';
 
 const queryClient = new QueryClient();
 
@@ -11,17 +12,19 @@ function App() {
   return (
     <div>
       <QueryClientProvider client={queryClient}>
-        <BrowserRouter>
-          <Routes>
-            <Route element={<MainLayout />}>
-              <Route path="/" element={<Home />} />
-              <Route path="/starred" element={<Starred />} />
-            </Route>
-            <Route path="/show/:showId" element={<Show />} />
-            <Route path="/contact" element={<div>contact page</div>} />
-            <Route path="*" element={<div>Page Not Found</div>} />
-          </Routes>
-        </BrowserRouter>
+        <GlobalTheme>
+          <BrowserRouter>
+            <Routes>
+              <Route element={<MainLayout />}>
+                <Route path="/" element={<Home />} />
+                <Route path="/starred" element={<Starred />} />
+              </Route>
+              <Route path="/show/:showId" element={<Show />} />
+              <Route path="/contact" element={<div>contact page</div>} />
+              <Route path="*" element={<div>Page Not Found</div>} />
+            </Routes>
+          </BrowserRouter>
+        </GlobalTheme>
       </QueryClientProvider>
     </div>
   );
--- src/components/SearchFrom.jsx
+++ src/components/SearchFrom.jsx
@@ -1,12 +1,11 @@
-import {  useState } from 'react';
+import { useState } from 'react';
 import { useSearchStr } from '../lib/useSearchStr';
+import CustomRadio from './CustomRadio';
 
 export default function SearchForm({ onSearch }) {
   const [searchString, setSearchString] = useSearchStr();
   const [searchOption, setSearchOption] = useState('shows');
 
-
-  
   const onInputChange = evt => {
     // console.log(evt.target.value);
     // console.log(searchString);
@@ -29,48 +28,43 @@ export default function SearchForm({ onSearch }) {
   return (
     <form onSubmit={onSubmit}>
       <input type="text" onChange={onInputChange} value={searchString} />
-      <label htmlFor="">
-        Shows
-        <input
-          type="radio"
-          value="shows"
-          name="search-option"
-          onChange={onRadioChange}
-          checked={searchOption === 'shows'}
-        />
-      </label>
-      <label htmlFor="">
-        Actors
-        <input
-          type="radio"
-          value="actors"
-          name="searh-option"
-          onChange={onRadioChange}
-          checked={searchOption === 'actors'}
-        />
-      </label>
+
+      <CustomRadio
+        label="Shows"
+        value="shows"
+        name="search-option"
+        onChange={onRadioChange}
+        checked={searchOption === 'shows'}
+      />
+
+      <CustomRadio
+        label="Actors"
+        value="actors"
+        name="searh-option"
+        onChange={onRadioChange}
+        checked={searchOption === 'actors'}
+      />
+
       <button type="submit">Search</button>
     </form>
   );
 }
 
-
-
 //1)mount
-  // //2)rerender
-  // //2.5)logic before next rerender
-  // //3)unmounts
-  // console.log('rerender');
-  // //useEffect runs atleast once
-  // useEffect(() => {
-  //   // console.log('mounted');
-  //   console.log('search option changes', searchOption);
+// //2)rerender
+// //2.5)logic before next rerender
+// //3)unmounts
+// console.log('rerender');
+// //useEffect runs atleast once
+// useEffect(() => {
+//   // console.log('mounted');
+//   console.log('search option changes', searchOption);
 
-  //   return () => {
-  //     console.log('before next useEffect run', searchOption);
-  //   };
+//   return () => {
+//     console.log('before next useEffect run', searchOption);
+//   };
 
-  //   // return () => {                  //cleanup function
-  //   //   console.log("unmounted");     //only runs if there is no dependencies
-  //   // };
-  // }, [searchOption]);
+//   // return () => {                  //cleanup function
+//   //   console.log("unmounted");     //only runs if there is no dependencies
+//   // };
+// }, [searchOption]);
